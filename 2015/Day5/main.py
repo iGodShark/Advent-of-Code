115fca5a8d8a5c350a4371d65118161bbd08b035
iGodShark <ben.s.kaplun@gmail.com>
2022-07-29 18:51:47 -0600

    2015 Day 5 Part 2

--- 2015/Day5/main.py
+++ 2015/Day5/main.py
@@ -30,6 +30,7 @@ def isNice(str):
     
     return False
 
+# Day 1
 niceStrings = 0
 
 for str in strings:
@@ -37,3 +38,34 @@ for str in strings:
         niceStrings += 1
 
 print("Day 1 Solution:", niceStrings, "Nice Strings.")
+
+# is the string nice day2?
+def isNiceDay2(str):
+    # must contain a 3-digit palindrome
+    # and contain 2 pairs of 2 characters without overlapping
+    containsPalindrome = False
+    for i in range(len(str) - 2):
+        if str[i] == str[i+2]:
+            containsPalindrome = True
+            break
+    
+    if not containsPalindrome:
+        return False
+    
+    # must contain 2 pairs of 2 characters without overlapping
+    for i in range(len(str) - 3):
+        strPair = str[i] + str[i+1]
+        for j in range(i + 2, len(str) - 1):
+            if strPair == str[j] + str[j+1]:
+                return True
+    
+    return False
+
+# Day 2
+niceStringsDay2 = 0
+
+for str in strings:
+    if (isNiceDay2(str)):
+        niceStringsDay2 += 1
+
+print("Day 2 Solution:", niceStringsDay2, "Nice Strings.")
